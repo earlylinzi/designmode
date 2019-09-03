30c0da96947b63c85a339ecdf9b37bb5c587d54a
80002523 <80002523@sf-express.com>
2019-09-03 23:07:03 +0800

    duoxinagchegn ceshi

--- src/main/java/com/early/duoxiancheng/PlusThread.java
+++ src/main/java/com/early/duoxiancheng/PlusThread.java
@@ -1,15 +1,17 @@
 package com.early.duoxiancheng;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 线程类
  */
 public class PlusThread implements Runnable{
 
-    public static int i;
+    public static  AtomicInteger i = new AtomicInteger(0);
 
     public void run() {
         for (int j = 0; j < 1000; j++) {
-            i = i++;
+            i.getAndIncrement();
         }
     }
 }
--- src/main/java/com/early/duoxiancheng/Test001.java
+++ src/main/java/com/early/duoxiancheng/Test001.java
@@ -7,11 +7,13 @@ import org.junit.Test;
  */
 public class Test001 {
 
-    static int count = 0;
 
+    /**
+     * 测试十个线程同时对一个变量进行++操作1000次
+     */
     @Test
     public void test001(){
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             Thread thread = new Thread(new PlusThread());
             thread.start();
         }
